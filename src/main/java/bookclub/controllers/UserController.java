b8bd3cd1fb3c27770594ae7f0aa5772dca065aec
Christopher Punt <christopherpunt@outlook.com>
2023-03-14 22:46:30 -0400

    add login and register pages

--- src/main/java/bookclub/controllers/UserController.java
+++ src/main/java/bookclub/controllers/UserController.java
@@ -3,15 +3,18 @@ package bookclub.controllers;
 import bookclub.models.User;
 import bookclub.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController
-@RequestMapping("/api")
+@Controller
+//@RequestMapping("/api")
 public class UserController {
 
     @Autowired
@@ -26,4 +29,29 @@ public class UserController {
     public List<User> getUsers(){
         return userService.getUsers();
     }
+
+    @RequestMapping("/login")
+    public String login(Model model){
+        model.addAttribute("user", new User());
+        return "login";
+    }
+
+    @RequestMapping("/register")
+    public String showRegistrationForm(Model model){
+        model.addAttribute("user", new User());
+
+        return "signup_form";
+    }
+
+    @PostMapping("/process_register")
+    public String processRegister(User user) {
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        String encodedPassword = passwordEncoder.encode(user.getPassword());
+        user.setPassword(encodedPassword);
+
+        userService.createUser(user);
+
+        return "register_success";
+    }
+
 }
