ef28a7c085964dacfa430350abb97e99777fc1cb
Christopher Punt <christopherpunt@outlook.com>
2023-03-24 17:28:53 -0400

    made popup alert look nicer and fixed some errors that were happening in the browser

--- src/main/java/bookclub/controllers/BookController.java
+++ src/main/java/bookclub/controllers/BookController.java
@@ -24,11 +24,6 @@ public class BookController {
     @Autowired
     BookService bookService;
 
-//    @RequestMapping(value= "/book", method= RequestMethod.POST)
-//    public Book createBook(@RequestBody Book book){
-//        return bookService.createBook(book);
-//    }
-
     @RequestMapping(value= "/bookisbn", method= RequestMethod.POST)
     public Book createBookFromIsbn(@RequestBody String isbn){
         return bookService.createBook(BookService.getBookDetails(isbn));
@@ -60,14 +55,12 @@ public class BookController {
         Optional<User> user = userDao.findByEmail(principal.getName());
         if (user.isPresent()) {
             Book book = GoogleBookDetailsService.getBookDetailsFromIsbn(bookItem);
-            //TODO:replace with optional
             assert book != null;
             book.setUser(user.get());
-            //TODO: book wont save if the description is too long, so set it to blank for now
             book.setDescription("");
             bookService.createBook(book);
             return ResponseEntity.ok("Book added to library successfully");
         }
-        return ResponseEntity.badRequest().body("User not found");
+        return ResponseEntity.badRequest().body("Failed to Add book to library");
     }
 }
