5a42d8e66a69f8f0b7fba82fd0b37b1fd7de874e
Christopher Punt <christopherpunt@outlook.com>
2023-04-03 23:22:27 -0400

    add notification entity

---
+++ src/main/java/bookclub/models/Notification.java
@@ -0,0 +1,37 @@
+package bookclub.models;
+
+import bookclub.enums.NotificationStatus;
+import bookclub.enums.NotificationType;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class Notification {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private User user;
+
+    @ManyToOne
+    private User sender;
+
+    @Column
+    @Enumerated(EnumType.STRING)
+    private NotificationType notificationType;
+
+    @Column
+    @Enumerated(EnumType.STRING)
+    private NotificationStatus status;
+
+    @Column
+    private String notificationData;
+
+    @Column
+    private String action;
+
+}
