7a26d4732234da8184602189d69d92c9a86c5ca3
Christopher Punt <christopherpunt@outlook.com>
2023-04-03 23:23:12 -0400

    create notification contoller

---
+++ src/main/java/bookclub/controllers/NotificationController.java
@@ -0,0 +1,16 @@
+package bookclub.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.security.Principal;
+
+@Controller
+public class NotificationController {
+
+    @GetMapping("/notifications")
+    public String getNotifications(Model model, Principal principal){
+        return "notifications";
+    }
+}
