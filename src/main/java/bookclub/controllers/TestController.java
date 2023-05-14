f8ef1451f5ee23ad6e2c0a1f96e795cdddb15b6b
Christopher Punt <christopherpunt@outlook.com>
2023-05-13 23:23:09 -0400

    comment out test controller

--- src/main/java/bookclub/controllers/TestController.java
+++ src/main/java/bookclub/controllers/TestController.java
@@ -1,12 +1,11 @@
 package bookclub.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class TestController {
-    @GetMapping("/testpage")
-    public String testPage(){
-        return "testPage";
-    }
+//    @GetMapping("/testpage")
+//    public String testPage(){
+//        return "testPage";
+//    }
 }
